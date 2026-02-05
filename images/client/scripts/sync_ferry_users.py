bb882fd199b89868e55528427ed7ea628210af20
Dennis Lee <dylee@fnal.gov>
2026-02-05 14:42:09 -0600

    client: Refactor and update sync_ferry_users.py
    Refactored sync_ferry_users.py to reduce calls to Rucio and Ferry
    Update sync_ferry_users to use the `config.py`. `config.py` has a get
    function that acts like a singleton. This allows the config to be called
    from any point in the script.

--- images/client/scripts/sync_ferry_users.py
+++ images/client/scripts/sync_ferry_users.py
@@ -8,23 +8,35 @@ Also applies analysis account attributes/policy to these accounts.
 The DN of the cert used to access FERRY needs Read-only access
 """
 
-import argparse
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
 import logging
 import os
 import sys
 from typing import Union
 
 from rucio.client import Client as RucioClient
-from rucio.common.exception import AccountNotFound, Duplicate
+from rucio.common.exception import (AccountNotFound,
+                                    Duplicate,
+                                    RSENotFound)
 
-from FerryClient import FerryClient, UserLDAPError
+import config
+from FerryClient import (FerryClient,
+                         UserLDAPError,
+                         create_ferry_client,
+                         get_ferry_client)
+
+log_level = config.get("log_level", "info")
+print(log_level)
+if log_level == "info":
+    level = logging.INFO
+if log_level == "debug":
+    level = logging.DEBUG
 
 # setup logger
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(level)
 ch = logging.StreamHandler(stream=sys.stdout)
-ch.setLevel(logging.INFO)
+ch.setLevel(level)
 logger.addHandler(ch)
 
 # analysis account attributes
@@ -53,8 +65,10 @@ class User:
     a Rucio Account
     """
     name: str
+    rucio_name: str
     # email: str
     identities: list[Identity]
+    scope: str
     # uuid: str
     # issuer: str
 
@@ -68,26 +82,55 @@ def get_email(ferry: FerryClient, username: str) -> str:
         raise UserLDAPError(e)
 
 
-def sync_ferry_users(commit=False,
-                     delete_accounts=False,
-                     add_scopes=False,
-                     add_analysis=False,
-                     vo='int'):
+def gather_ferry_identities(user: dict, user_dns: list) -> list[Identity]:
+    """Gather FERRY Identities"""
+    token_iss = config.get("token_issuer", None)
+
+    identities: list[Identity] = []
+
+    # OIDC identities
+    # Only inactive users have no token
+    if token_iss and user.get('tokensubject', None):
+        identity = Identity(id_type='OIDC',
+                            identity={'sub': user['tokensubject'],
+                                      'iss': token_iss})
+        identities.append(identity)
+
+    # get x509 identities
+    if user_dns:
+        dns = user_dns[0]['certificates']
+        for dn in dns:
+            identity = Identity(id_type='X509',
+                                identity=dn['dn'])
+            identities.append(identity)
+    else:
+        logger.error(f"No dns for {user['username']} found")
+
+    return identities
+
+
+def main():
     """
     Fetches users from FERRY and adds them to Rucio with analysis attributes
     """
     # setup clients
-    ferry = FerryClient(logger=logger)
+    create_ferry_client(logger=logger)
+
+    ferry = get_ferry_client()
     client = RucioClient()
 
-    unitname = os.getenv("FERRY_VO", vo)
+    unitname = config.get("vo", "int")
+    username_format = config.get("username_format", "{}")
+    scope_format = config.get("scope_format", "user.{}")
+    from_ferry = config.get("from_ferry", False)
+    commit = config.get("commit", False)
+    delete_accounts = config.get("delete_accounts", False)
+
     filtered_users = os.getenv("FILTER_USERS", None)
-    token_iss = os.getenv("TOKEN_ISS", None)
 
     # get all members and all DNs for an affiliation
     try:
         members = ferry.getAffiliationMembers(unitname)[0]
-        all_dns = ferry.getAllUsersCertificateDNs(unitname)
     except Exception as e:
         logger.error("Could not get users in affiliation %s", unitname)
         logger.error(e)
@@ -98,9 +141,22 @@ def sync_ferry_users(commit=False,
         filtered = filtered_users.split(',')
         members['users'] = [m for m in members['users'] if m['username'] in filtered]
 
+    # get DNs if from Ferry
+    all_dns = []
+    if from_ferry:
+        try:
+            all_dns = ferry.getAllUsersCertificateDNs(unitname)
+        except Exception as e:
+            logger.error("Could not get all_dns in affiliation %s", unitname)
+            logger.error(e)
+            raise
+
     users_to_add = [] 
     for user in members['users']:
         username = user['username']
+        rucio_name = username_format.format(username)
+
+        scope = scope_format.format(username)
 
         # ignore banned or deactivated users
         try:
@@ -111,34 +167,24 @@ def sync_ferry_users(commit=False,
             continue
 
         # Add identities
-        identities: list[Identity] = []
-
-        # OIDC identities
-        # Only inactive users have no token
-        if token_iss and user.get('tokensubject', None):
-            identity = Identity(id_type='OIDC',
-                                identity={'sub': user['tokensubject'],
-                                          'iss': token_iss})
-            identities.append(identity)
-
-        # get x509 identities
-        user_dns = list(filter(lambda x: x['username'] == username, all_dns))
-        if user_dns:
-            dns = user_dns[0]['certificates']
-            for dn in dns:
-                identity = Identity(id_type='X509',
-                                    identity=dn['dn'])
-                identities.append(identity)
+        if from_ferry:
+            user_dns = list(filter(lambda x: x['username'] == username, all_dns))
+            identities: list[Identity] = gather_ferry_identities(user,
+                                                                 user_dns)
         else:
-            logger.error(f"No dns for {username} found")
+            config_ids = config.get("identities", [])
+            identities = [Identity(**d) for d in config_ids]
 
-        users_to_add.append(User(name=username, identities=identities))
+        users_to_add.append(User(name=username,
+                                 rucio_name=rucio_name,
+                                 scope=scope,
+                                 identities=identities))
     
     # Add or update users to Rucio
     for user in users_to_add:
-        logger.info("Adding user %s", user.name)
+        logger.info("Adding user %s with %s", user.name, user.rucio_name)
         if commit:
-            add_user(ferry, client, user, add_scopes, add_analysis)
+            sync_user(client, user)
         else:
             logger.info("User had %s identities to add", len(user.identities))
             logger.debug("User identities: %s", user.identities)
@@ -148,11 +194,14 @@ def sync_ferry_users(commit=False,
         delete_users(client, members, commit)
 
 
-def add_user(ferry: FerryClient, client: RucioClient, user: User, add_scopes=False, add_analysis=False):
+def sync_user(client: RucioClient, user: User):
     """
-    Add users to Rucio
+    Add or sync users to Rucio
     """
-    username = user.name
+    ferry = get_ferry_client()
+
+    name = user.name
+    username = user.rucio_name
     email = ''
     try:
         account = client.get_account(username)
@@ -160,9 +209,9 @@ def add_user(ferry: FerryClient, client: RucioClient, user: User, add_scopes=Fal
     except AccountNotFound:
         logger.info(f"Creating account for {username}")
         try:
-            email = get_email(ferry, username)
+            email = get_email(ferry, name)
         except UserLDAPError as e:
-            logger.error(f"Could not get userLdapInfo for {username}, skipping")
+            logger.error("Could not get userLdapInfo for %s, skipping", name)
             logger.error(e)
             return
         client.add_account(username, 'USER', email)
@@ -203,20 +252,40 @@ def add_user(ferry: FerryClient, client: RucioClient, user: User, add_scopes=Fal
             logger.debug("Identity exists %s", identity_str)
 
     # create a scope
-    if add_scopes:
-        logger.info(f"Adding scope for user: {username}")
+    if config.get('add_scopes', False):
+        logger.info("Adding scope for user: %s", username)
         try:
-            client.add_scope(username, f'user.{username}')
+            client.add_scope(username, user.scope)
         except Duplicate:
             logger.info(f"Scope for user {username} already exists")
 
     # add attributes, default False
-    if add_analysis:
+    if config.get('add_attributes', False):
         logger.info(f"Adding analysis attributes")
         for a in ANALYSIS_ATTRIBUTES:
             try:
                 client.add_account_attribute(username, a, "1")
             except Duplicate as e:
+                logger.error(e)
+                continue
+
+    if config.get("set_limits", False):
+        logger.info("Setting RSE limits")
+        limits = config.get("rse_limits", {})
+        for rse, limit in limits.items():
+            try:
+                client.set_account_limit(username,
+                                         rse,
+                                         limit,
+                                         "local")
+            except RSENotFound as e:
+                logger.error("Could not set limits on %s for %s",
+                             rse, username)
+                logger.error(e)
+                continue
+            except Exception as e:
+                logger.error("Could not set limits on %s for %s",
+                             rse, username)
                 logger.error(e)
                 continue
 
@@ -239,33 +308,6 @@ def delete_users(client: RucioClient, members, commit=False):
                 logger.info(f"Disabling account {a}, FERRY disabled or banned")
                 if commit:
                     client.delete_account(a)
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='Sync FERRY Users',
-        epilog='Syncs FERRY Users of a VO with Rucio')
-    parser.add_argument('--commit',
-                        help='commit users to Rucio',
-                        action='store_true')
-    parser.add_argument('--delete_accounts',
-                        help='allow deleting/disabling of accounts. --commit is required',
-                        action='store_true')
-    parser.add_argument('--add_scopes',
-                        help='add user scope',
-                        dest='scopes',
-                        action='store_true')
-    parser.add_argument('--add_analysis_attributes',
-                        help=f'add the following analysis account attributes: {ANALYSIS_ATTRIBUTES}',
-                        dest='analysis',
-                        action='store_true')
-
-    args = parser.parse_args()
-
-    sync_ferry_users(commit=args.commit,
-                     delete_accounts=args.delete_accounts,
-                     add_scopes=args.scopes,
-                     add_analysis=args.analysis)
 
 
 if __name__ == "__main__":
